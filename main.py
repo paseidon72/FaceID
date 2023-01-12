dc023ab6e7e7395664a79dc6837692d34774c9fc
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2023-01-12 18:27:28 +0200

    faceid

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from deepface import DeepFace
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def face_verify(img_1, img_2):
+    try:
+        result_dict = DeepFace.verify(img1_path=img_1, img2_path=img_2)
+        
+        with open('result.json', 'w') as file:
+            json.dump(result_dict, file, indent=4, ensure_ascii=False)
+        
+        # return result_dict
+        
+        if result_dict.get('verified'):
+            return 'Проверка пройдена. Пропустить.'
+        return 'Нарушитель! Задержать!!!'
+    
+    except Exception as _ex:
+        return _ex
+    
+    
+def face_recogn():
+    try:
+        result = DeepFace.find(img_path='faces/jim.jpg', db_path='Emilia')
+        result = result.values.tolist()
+        
+        return result
+    
+    except Exception as _ex:
+        return _ex
+    
 
+def face_analyze():
+    try:
+        # result_dict = DeepFace.analyze(img_path='faces/jim.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        # result_dict = DeepFace.analyze(img_path='faces/adr.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        result_dict = DeepFace.analyze(img_path='faces/snoop.jpg', actions=['age', 'gender', 'race', 'emotion'])
+        
+        with open('face_analyze.json', 'w') as file:
+            json.dump(result_dict, file, indent=4, ensure_ascii=False)
+            
+        print(f'[+] Age: {result_dict.get("age")}')
+        print(f'[+] Gender: {result_dict.get("gender")}')
+        print('[+] Race:')
+        
+        for k, v in result_dict.get('race').items():
+            print(f'{k} - {round(v, 2)}%')
+            
+        print('[+] Emotions:')
+        
+        for k, v in result_dict.get('emotion').items():
+            print(f'{k} - {round(v, 2)}%')
+            
+        # return result_dict
 
-# Press the green button in the gutter to run the script.
+    except Exception as _ex:
+        return _ex
+
+    
+def main():
+    # print(face_verify(img_1='faces/em1.jpg', img_2='faces/snoop.jpg'))
+    # print(face_recogn())
+    face_analyze()
+    
+    
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
